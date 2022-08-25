f2372505d9fd3e27dd2296f791fe7a580f59ca01
Maxwell Villaire <mvillaire@LUMA-M0040.local>
2022-08-25 15:05:55 -0500

    add empty components

---
+++ src/components/DealerHand/DealerHand.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './DealerHand.css';
+
+function DealerHand() {
+  return (
+    <div className="DealerHand">
+    </div>
+  );
+}
+
+export default DealerHand;
---
+++ src/components/PlayerHand/PlayerHand.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './PlayerHand.css';
+
+function PlayerHand() {
+  return (
+    <div className="PlayerHand">
+
+    </div>
+  );
+}
+
+export default PlayerHand;
