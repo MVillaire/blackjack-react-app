264318adfe3cd6ca21a92651932021dac9b71781
Maxwell Villaire <mvillaire@LUMA-M0040.local>
2022-08-25 15:08:32 -0500

    add table component

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Table.css';
+
+function Table() {
+  return (
+    <div className="Table">
+
+    </div>
+  );
+}
+
+export default Table;
