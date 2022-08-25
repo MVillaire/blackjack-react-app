16f3f18e55e1454f84ca2f976e95f82bda7238f6
Maxwell Villaire <mvillaire@LUMA-M0040.local>
2022-08-25 15:23:06 -0500

    fix imports

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './Card.css';
 
+// const Card = {
+//   "image": "https://www.deckofcardsapi.com/static/img/KH.png",
+//   "value": "KING",
+//   "suit": "HEARTS",
+//   "code": "KH",
+//   "showing": false
+// },
+
 function Card() {
   return (
     <div className="Card">
